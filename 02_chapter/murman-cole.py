51316debb450ed710bae9a4a1a1b5f1cfe398c0b
ota0410 <s1411506@klis.tsukuba.ac.jp>
2025-05-20 23:58:46 +0900

    add script murman-cole.py

---
+++ 02_chapter/murman-cole.py
@@ -0,0 +1,50 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def init(q1, q2, dx, jmax):
+    xs = -1.0
+    x = np.linspace(xs, xs+dx*(jmax-1), jmax)
+    q = np.array([(float(q1) if i<0.0 else float(q2)) for i in x])
+    return (x, q)
+
+def do_computing(x, q, dt, dx, nmax, ff, order=1, interval=2):
+    plt.figure(figsize=(7,7), dpi=100)
+    plt.rcParams["font.size"] = 22
+
+    plt.plot(x, q, marker='o', lw=2, label='n=0')
+
+    for n in range(1, nmax+1):
+        qold = q.copy()
+        for j in range(order, jmax-order):
+            ff1 = ff(qold, qold[j], dt, dx, j)
+            ff2 = ff(qold, qold[j], dt, dx, j-1)
+            q[j] = qold[j] - dt/dx * (ff1 - ff2)
+            
+        if n % interval == 0:
+            plt.plot(x, q, marker='o', lw=2, label=f'n={n}')
+            
+    plt.grid(color='black', linestyle='dashed', linewidth=0.5)
+    plt.xlabel('x')
+    plt.ylabel('q')
+    plt.legend()
+    plt.show()
+
+def MC(q, c, dt, dx, j):
+    ur = q[j+1]
+    ul = q[j]
+    fr = 0.5 * ur**2
+    fl = 0.5 * ul**2
+    c = 0.5 * (ur + ul)
+    return 0.5 * (fr + fl - np.sign(c) * (fr - fl))
+
+c = 1
+dt = 0.05
+dx = 0.1
+
+jmax = 41
+nmax = 20
+
+q1 = 1
+q2 = 0
+x, q = init(q1, q2, dx, jmax)
+do_computing(x, q, dt, dx, nmax, MC, interval=5)
